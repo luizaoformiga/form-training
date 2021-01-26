254f723ae9af0355756774bbc5e8169c8ec371ff
Nome Sobrenome <pereiralimaluizpaulo7@gmail.com>
2021-01-26 13:27:14 -0200

    add

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 function goHere(){
-    var nameValue = document.querySelector(".js-input-name").value;
-    var emailValue = document.querySelector(".js-input-email").value;
-    var passwordValue = document.querySelector(".js-input-password").value; 
+    const nameValue = document.querySelector(".js-input-name").value;
+    const emailValue = document.querySelector(".js-input-email").value;
+    const passwordValue = document.querySelector(".js-input-password").value; 
 
-    var formValue = {                                 
+    const formValue = {                                 
         name: nameValue,
         email: emailValue,
         data: {                                     
